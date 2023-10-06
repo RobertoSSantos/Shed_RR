e6b40995062a0c2e4d4e73126b829a3f1db2778c
RobertoSSantos <rssan03@gmail.com>
2023-10-06 10:07:12 -0300

    Features Added

--- shed_rr.c
+++ shed_rr.c
@@ -2,9 +2,10 @@
 #include <pthread.h>
 #include <math.h>
 #include <unistd.h>
+#include <time.h>
 
-//Compilar - gcc -o alias shed_rr.c -lm
-//Run - ./alias
+//Para Compilar executar em terminal -> gcc -o alias shed_rr.c -lm
+//Para executar em terminal -> ./alias
 
 pthread_t thread_0, thread_1, thread_2, thread_3, thread_4;
 
@@ -23,28 +24,123 @@ int main(){
     pthread_create(&thread_4, NULL, (void *) task_4, NULL);
 
     //Finaliza todas as threads quando a thread especificada termina e tem um retorno especificado
+    pthread_join(thread_0, NULL);
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
+    pthread_join(thread_3, NULL);
     pthread_join(thread_4, NULL);
 
     return 0;
 }
 
-void* task_0(void *param){
-    // set 
+void* task_0(){
+    clock_t start_t, end_t;
+    struct sched_param param;
+
+    // Definicao da prioridade
+    param.sched_priority = 10;
+
+    start_t = clock();
+
+    //Configurar o schedule
+    pthread_setschedparam(thread_0, SCHED_RR, &param);
+
+    // Inicio do processo 
+    
+
+    end_t = clock();
+    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Tempo de execucao thread 0: %f\n ", total_t  );
+
+    //Finalizando a thread
     pthread_exit(NULL);
 }
 
-void* task_1(void *param){
+void* task_1(){
+    clock_t start_t, end_t;
+    struct sched_param param;
+
+    // Definicao da prioridade
+    param.sched_priority = 70;
+
+    start_t = clock();
+
+    //Configurar o schedule
+    pthread_setschedparam(thread_1, SCHED_RR, &param);
+
+    // Inicio do processo 
+
+
+    end_t = clock();
+    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Tempo de execucao thread 1: %f\n ", total_t  );
+
+    //Finalizando a thread
     pthread_exit(NULL);
 }
 
-void* task_2(void *param){
+void* task_2(){
+    clock_t start_t, end_t;
+    struct sched_param param;
+
+    // Definicao da prioridade
+    param.sched_priority = 30;
+
+    start_t = clock();
+
+    //Configurar o schedule
+    pthread_setschedparam(thread_2, SCHED_RR, &param);
+
+    // Inicio do processo 
+
+    end_t = clock();
+    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Tempo de execucao thread 2: %f\n", total_t  );
+
+    //Finalizando a thread
     pthread_exit(NULL);
 }
 
-void* task_3(void *param){
+void* task_3(){
+    clock_t start_t, end_t;
+    struct sched_param param;
+
+    // Definicao da prioridade
+    param.sched_priority = 80;
+
+    start_t = clock();
+
+    //Configurar o schedule
+    pthread_setschedparam(thread_3, SCHED_RR, &param);
+
+    // Inicio do processo 
+
+    end_t = clock();
+    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Tempo de execucao thread 3: %f\n", total_t  );
+
+    //Finalizando a thread
     pthread_exit(NULL);
 }
 
-void* task_4(void *param){
+void* task_4(){
+    clock_t start_t, end_t;
+    struct sched_param param;
+
+    // Definicao da prioridade
+    param.sched_priority = 25;
+
+    start_t = clock();
+
+    //Configurar o schedule
+    pthread_setschedparam(thread_4, SCHED_RR, &param);
+
+    // Inicio do processo 
+
+    end_t = clock();
+    double total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Tempo de execucao thread 4: %f\n", total_t  );
+
+    //Finalizando a thread
     pthread_exit(NULL);
 }
